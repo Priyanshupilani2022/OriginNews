95ab882c0696efce25ff49b82a854a354072ccde
Priyanshupilani2022 <109089571+Priyanshupilani2022@users.noreply.github.com>
2023-08-04 17:31:48 +0530

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "no-unused-vars": "off", // <- Add a comma here to separate the rules
+    "prettier/prettier": 0,
+  },
+};
